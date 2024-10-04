27a834560f25dab68808ee4b1f89cb89ca70d7c8
juanasacco <jua.sacco04@gmail.com>
2024-10-04 10:55:26 -0300

    Documente la funcion suma

--- R/suma.R
+++ R/suma.R
@@ -1,13 +1,27 @@
-#Suma dos numeros
-
+#' Suma dos numeros
+#'
+#'La funcion `suma()` toma 2 numeros positivos y los suma
+#'
+#'Estos son detalles de la función
+#'
+#' @param x un vector numérico
+#' @param y un vector numérico
+#'
+#' @return un vector numérico con la suma de `x` e `y`
+#'
+#' @examples
+#' suma(2,2)
+#' sum(15,7)
+#'
+#' @export
 suma <- function(x = 0, y = 0) {
-  # Chequear si ambos argumentos son numéricos
+  # Chequea si son numéricos
   if (!is.numeric(x) | !is.numeric(y)) {
-    cli::cli_abort("Los argumentos deben ser numéricos.")
+    cli::cli_abort("Los argumentos deben ser numéricos")
   }
-  # Chequear si x o y son negativos
+  # Chequea si los numeros son negativos
   if (x < 0 | y < 0) {
-    cli::cli_abort("No puedo sumar negativos.")
+    cli::cli_abort("No puedo sumar negativos")
   }
   return(x + y)
 }
